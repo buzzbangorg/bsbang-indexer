7b666f832a0dab9655a2fe74aebcc33870acc09b
innovationchef <ankit.lohani.iitkgp@gmail.com>
2018-07-10 00:06:08 +0530

    multithreading not working - TBD Later

--- bsbang-index.py
+++ bsbang-index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import timeit
+import time
 import logging
 import requests
-from multiprocessing import Process, Queue
+import threading
 from bioschemas_indexer import indexer
 
 
@@ -28,7 +29,6 @@ def flatten(d, bioschemas):
                         # TBD LATER
                 else:
                     sendSolr[stype + '.' + key] = value
-            # prettyprint(sendSolr)
             struct = {**struct, **sendSolr}
 
     if isinstance(d['@type'], list):
@@ -38,31 +38,35 @@ def flatten(d, bioschemas):
         open(d, d['@type'], bioschemas)
 
 
-def collect_data(mongodb, q):
-    n = 1000
+def collect_data(mongodb):
+    n = 100
     global last_id
     global struct
+    global n_docs
+    global q
     collection = indexer.connect_db(mongodb)
+    n_docs = collection.count()
     bioschemas = indexer.get_speclist()
-    len_db = 50
-    for i in range(len_db):
-        data, last_id = indexer.read_mongodb(
-            collection, page_size=n, last_id=last_id)
+    data, last_id = indexer.read_mongodb(
+        collection, page_size=n, last_id=last_id)
+    for i in range(int(n_docs / n)+1):
         for item in data:
             flatten(item, bioschemas)
-            q.put(struct)
+            q.append(struct)
             struct = {}
-        logger.info("collected %d docs", n)
-        i += 1
-    q.put(False)
+# 106713
 
-
-def index_data(solr, q):
+def index_data(solr):
+    global q
+    global flag
+    global n_docs
     while True:
-        # print(q.qsize())
-        jsonld = []
-        for i in iter(q.get, False):
-            jsonld.append(i)
+        if len(q) == 0:
+            continue
+        jsonld = q[flag:-1]
+        print("->", flag, len(q)-1)
+        flag = len(q)
+        # time.sleep(1)
         solr_endpoint = 'http://' + solr['SOLR_SERVER'] + ':' + \
             solr['SOLR_PORT'] + '/solr/' + solr['SOLR_CORE'] + '/'
         headers = {'Content-type': 'application/json'}
@@ -71,27 +75,23 @@ def index_data(solr, q):
                           '?commit=true', json=jsonld, headers=headers)
         if r.status_code != 200:
             logger.error('Could not post to Solr: %s', r.text)
-        if q.empty() == True:
+        if flag>n_docs:
             break
 
 
 # MAIN
-############################################################
 mongodb, solr = indexer.read_conf()
 struct = {}
 last_id = None
-############################################################
-start_time = timeit.default_timer()
-############################################################
-q = Queue()
-process_one = Process(target=collect_data, args=(mongodb, q))
-process_two = Process(target=index_data, args=(solr, q))
-process_one.start()
-process_two.start()
-q.close()
-q.join_thread()
+n_docs = 0
+flag = 0
+q = list()
 
-process_one.join()
-process_two.join()
-############################################################
+start_time = timeit.default_timer()
+T1 = threading.Thread(target=collect_data, args=(mongodb,))
+T2 = threading.Thread(target=index_data, args=(solr,))
+T1.start()
+T2.start()
+T1.join()
+T2.join()
 print(timeit.default_timer() - start_time)
