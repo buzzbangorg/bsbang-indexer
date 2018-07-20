8573014ab652b877bb45c0c7618187663d0092e3
Justin Clark-Casey <justincc@justincc.org>
2018-07-20 17:36:43 +0100

    Get indexer to tell us how many docs it has indexed and the total

--- bsbang-index.py
+++ bsbang-index.py
@@ -51,12 +51,20 @@ def collect_data(mongodb):
         sendSolr.append(struct)
         struct = {}
 
-    logger.debug("collected %d docs", n)
+    logger.debug("Retrieved %d MongoDB entries", n)
 
     return sendSolr
 
 
 def index_data(solr, jsonld):
+    """
+    Index the given jsonld into the Solr instance.
+
+    :param solr:
+    :param jsonld:
+    :return: True on success, False on failure
+    """
+
     solr_endpoint = 'http://' + solr['SOLR_SERVER'] + ':' + \
         solr['SOLR_PORT'] + '/solr/' + solr['SOLR_CORE'] + '/'
     headers = {'Content-type': 'application/json'}
@@ -65,8 +73,9 @@ def index_data(solr, jsonld):
                       '?commit=true', json=jsonld, headers=headers)
     if r.status_code != 200:
         logger.error('Could not post to Solr: %s', r.text)
+        return False
     else:
-        logger.info("posted %d docs", len(jsonld))
+        return True
 
 
 # MAIN
@@ -75,9 +84,16 @@ struct = {}
 last_id = None
 collection = indexer.connect_db(mongodb)
 n_docs = collection.count()
+total_indexed = 0
+
+# logger.info('Indexing %d MongoDB entries', n_docs)
 
 start_time = timeit.default_timer()
 for i in range(int(n_docs / 1000)+1):
     sendSolr = collect_data(mongodb)
-    index_data(solr, sendSolr)
+
+    if index_data(solr, sendSolr):
+        total_indexed += len(sendSolr)
+        logger.info("Indexed %d of %d MongoDB entries", total_indexed, n_docs)
+
 print(timeit.default_timer() - start_time)
